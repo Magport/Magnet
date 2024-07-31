ba97dde3e56a22960934a8107ba0a70ba096542a
zachary0809 <139736437+zachary0809@users.noreply.github.com>
2024-07-31 14:16:48 +0800

    Fixed: excessive deductions
    When transferring money to an EVM account, a portion will be deducted to create a new account.

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -11,6 +11,7 @@ mod weights;
 pub mod xcm_config;
 pub mod xcms;
 
+use pallet_evm::AddressMapping;
 use codec::{Decode, Encode, MaxEncodedLen};
 use core::ops::Div;
 
@@ -362,7 +363,7 @@ pub const GRAND: Balance = QUID * 1_000;
 pub const MILLICENTS: Balance = CENTS / 1_000;
 
 /// The existential deposit. Set to 1/10 of the Connected Relay Chain.
-pub const EXISTENTIAL_DEPOSIT: Balance = MILLIUNIT;
+pub const EXISTENTIAL_DEPOSIT: Balance = 1;
 
 /// We assume that ~5% of the block weight is consumed by `on_initialize` handlers. This is
 /// used to limit the maximal weight of a single extrinsic.
@@ -1416,7 +1417,22 @@ impl_runtime_apis! {
 		}
 
 		fn account_basic(address: H160) -> EVMAccount {
-			let (account, _) = pallet_evm::Pallet::<Runtime>::account_basic(&address);
+			let account_id = <Runtime as pallet_evm::Config>::AddressMapping::into_account_id(address);
+	
+			let nonce = frame_system::Pallet::<Runtime>::account_nonce(&account_id);
+	
+			let balance_free =
+				<Runtime as pallet_evm::Config>::Currency::free_balance(account_id.clone());
+			let balance_reserved = 
+							<Runtime as pallet_evm::Config>::Currency::reserved_balance(account_id);
+			let balance = balance_free.saturating_add(balance_reserved);
+			
+	
+			let account = EVMAccount {
+					nonce: U256::from(UniqueSaturatedInto::<u128>::unique_saturated_into(nonce)),
+					balance: U256::from(UniqueSaturatedInto::<u128>::unique_saturated_into(balance)),
+			};
+			
 			account
 		}
 
